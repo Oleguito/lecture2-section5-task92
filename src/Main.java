b27fe2d50de2dda15ea9d1f244c44553d584baa7
Oleguito <tenzoriator@rambler.ru>
2023-08-13 16:38:28 +0300

    лекция 2 задание 5,92

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/* Вычислить сумму sqrt(1 + sqrt(2 + sqrt(3 + ... sqrt(50))) */
+
+public class Main {
+    /*  Число дано */
+    final static int N = 5;
+
+    public static void main(String[] args) {
+        System.out.println("getResult(1): " + getResult(1));
+    }
+
+    /*  Как проверить я не знаю но прилично уверен что работает правильно
+        Я проверил в экселе N = 5, сходится
+        Рекурсивная функция -- начинаем с n и поднимаемся до N где возвращаем последний корень
+    */
+    static double getResult(int n) {
+        if (n == N) {
+            return Math.sqrt(N);
+        } else {
+            return Math.sqrt(n + getResult(n + 1));
+        }
+    }
+}
